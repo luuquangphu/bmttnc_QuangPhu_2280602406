34857ce1d7e298bd75c79fdd2a70c4332fa2feed
Lưu Quang Phú <luuquangphuii@gmail.com>
2025-05-23 16:22:40 +0700

    [Add] vigenere

--- Buoi2/api.py
+++ Buoi2/api.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
+from cipher.vigenere import VigenereCipher
 from cipher.caesar import CaesarCipher
 
 app = Flask(__name__)
 
-# CAESAR CIPHER ALGORITHM
 caesar_cipher = CaesarCipher()
 
 @app.route("/api/caesar/encrypt", methods=["POST"])
@@ -22,6 +22,21 @@ def caesar_decrypt():
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
-# main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+@app.route("/api/vigenere/encrypt", methods=['POST'])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route("/api/vigenere/decrypt", methods=['POST'])
+def vigenere_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
